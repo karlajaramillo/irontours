4c0fa56f15c824d0bdba871409797fc97d72c3b7
Brigi Horvath <brigi.horvath82@gmail.com>
2021-12-04 15:38:12 +0100

    add back isLoggedOut middleware

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { getHome, getPrivate } = require('../controllers/index');
 const { isLoggedOut, isLoggedIn } = require('../middlewares/auth.middlewares');
 
-router.get('/', getHome).get('/private', isLoggedIn, getPrivate);
+router.get('/', isLoggedOut, getHome).get('/private', isLoggedIn, getPrivate);
 
 module.exports = router;
