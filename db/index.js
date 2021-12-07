b9fb4a085efaf509ee8d83150f4d74c81a5da3f1
Brigi Horvath <brigi.horvath82@gmail.com>
2021-12-07 19:45:23 +0100

    add MongoAtlas

--- db/index.js
+++ db/index.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-const { MONGODB_URL } = process.env;
+// const { MONGODB_URL } = process.env;
+const { DATABASE } = process.env;
 
 async function connectDb() {
   try {
-    const { connection } = await mongoose.connect(MONGODB_URL);
+    const { connection } = await mongoose.connect(DATABASE);
     console.log(`Conected to DB: ${connection.name}`);
   } catch (err) {
     console.error(`Error connecting to DB: ${err.message}`);
