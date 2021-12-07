05477ef89739c20356e0f851fa99772f2b84ad72
karlajaramillo <karlav.jaramillo@gmail.com>
2021-12-07 20:18:30 +0100

    Add mongo options

--- db/index.js
+++ db/index.js
@@ -5,7 +5,11 @@ const { MONGODB_URI } = process.env;
 
 async function connectDb() {
   try {
-    const { connection } = await mongoose.connect(MONGODB_URI);
+    const { connection } = await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
     console.log(`Conected to DB: ${connection.name}`);
   } catch (err) {
     console.error(`Error connecting to DB: ${err.message}`);
