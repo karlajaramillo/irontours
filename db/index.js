9a0920978812c2610b269f73c922972bc6eaab36
karlajaramillo <karlav.jaramillo@gmail.com>
2021-12-07 20:12:33 +0100

    Modify mongodb property

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 // const { MONGODB_URL } = process.env;
-const { DATABASE } = process.env;
+const { MONGODB_URI } = process.env;
 
 async function connectDb() {
   try {
-    const { connection } = await mongoose.connect(DATABASE);
+    const { connection } = await mongoose.connect(MONGODB_URI);
     console.log(`Conected to DB: ${connection.name}`);
   } catch (err) {
     console.error(`Error connecting to DB: ${err.message}`);
