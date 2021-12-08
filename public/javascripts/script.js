514f7a519d8507ba27b96d003034cfac90493493
Brigi Horvath <brigi.horvath82@gmail.com>
2021-12-08 21:42:27 +0100

    add profile page CSS

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,15 @@
 window.addEventListener('load', () => {
-    document.querySelector(".dropdown-button").addEventListener('click', event => {
-        //console.log(event.target.closest('.dropdown-button'));
-        document.querySelector('.dropdown-menu').classList.toggle('hidden');
-    })
-})
+  document
+    .querySelector('.dropdown-button')
+    .addEventListener('click', (event) => {
+      //console.log(event.target.closest('.dropdown-button'));
+      document.querySelector('.dropdown-menu').classList.toggle('hidden');
+    });
+  document.querySelector('#imageInput').addEventListener('change', (event) => {
+    const [file] = imageInput.files;
+    if (file) {
+      document.querySelector('.user-profile-image').src =
+        URL.createObjectURL(file);
+    }
+  });
+});
